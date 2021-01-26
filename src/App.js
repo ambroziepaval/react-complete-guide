ac9d4a4cf289e6a2c3a599ca3fc6ae80f6853cfe
ambroziepaval <ambrozie.paval@msg.group>
2021-01-26 15:04:26 +0200

    Removad styled components from app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`;
 
 class App extends Component {
   // whenever the state changes, the component will re-render and reflect the new state
@@ -108,7 +93,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Switch Name</StyledButton>
+        <button alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Switch Name</button>
         {persons}
       </div>
     );
